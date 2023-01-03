f13e4d8fd037c8ff5467c8318ecda87c5833ea3b
Gabriel Sá <gabriel.sa@laura-br.com>
2023-01-03 19:30:24 -0300

    Changed node version

--- packages/harun-ai-api/src/consumer/httpConsumer/koaHttpConsumer/server.ts
+++ packages/harun-ai-api/src/consumer/httpConsumer/koaHttpConsumer/server.ts
@@ -2,6 +2,6 @@ import app from './app';
 
 const port = process.env.PORT || 5000;
 
-app.listen(3333, () => {
+app.listen(3333, '0.0.0.0', () => {
   console.log(`Koa server started on port: ${port}`);
 });
